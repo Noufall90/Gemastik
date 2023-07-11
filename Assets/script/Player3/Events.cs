83be2d688bc4e87e4e71e9b794e51122a39c4eac
princenizroh <princenizroh7@gmail.com>
2023-07-11 11:14:50 +0800

    fixed bug

--- Assets/script/Player3/Events.cs
+++ Assets/script/Player3/Events.cs
@@ -5,7 +5,7 @@ public class Events : MonoBehaviour
 {
     public void ReplayGame()
     {
-        SceneManager.LoadScene("Game 3");
+        SceneManager.LoadScene("game3");
     }
     
     public void QuitGame()
