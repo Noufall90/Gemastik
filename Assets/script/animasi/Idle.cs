910e8465f42b347212d957931a0b5e1153832fbc
Noval ian <andrianau26@gmail.com>
2023-07-11 11:25:04 +0800

    1

--- Assets/script/animasi/Idle.cs
+++ Assets/script/animasi/Idle.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class Idle : StateMachineBehaviour
 {
-    [Serialfield]
+    [SerializeField]
+    private float _timeUntilIdle;
+
+    [SerializeField]
+    private int _numberOfIdle;
+
+    private bool _isIdle;
+    private float _idleTime;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+        ResetIdle(animator);
     }
 
-    OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+        if (!_isIdle)
+        {
+            _idleTime += Time.deltaTime;
+
+            if (_idleTime > _timeUntilIdle)
+            {
+                _isIdle = true;
+                int idleAnimation = Random.Range(1, _numberOfIdle + 1);
+
+                animator.SetFloat("IdleAnimation", idleAnimation);
+            }
+        }
+        else if (stateInfo.normalizedTime % 1 > 0.98f)
+        {
+            ResetIdle(animator);
+        }
     }
 
+    private void ResetIdle(Animator animator)
+    {
+        _isIdle = false;
+        _idleTime = 0f;
 
+        animator.SetFloat("IdleAnimation", 0f);
+    }
 }
