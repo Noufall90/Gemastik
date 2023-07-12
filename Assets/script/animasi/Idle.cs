0db34202940569ada8f953efab7b315d84b256ea
Noval ian <andrianau26@gmail.com>
2023-07-12 19:19:10 +0800

    1

--- Assets/script/animasi/Idle.cs
+++ Assets/script/animasi/Idle.cs
@@ -1,53 +1,50 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Idle : StateMachineBehaviour
-// {
-//     [SerializeField]
-//     private float _timeUntilIdle;
+public class Idle : StateMachineBehaviour
+{
+    [SerializeField]
+    private float _timeUntilIdle;
     
-//     [SerializeField]
-//     private int _numberOfIdle;
+    [SerializeField]
+    private int _numberOfIdle;
 
+    private bool _isIdle;
+    private float _idleTime;
 
-//     private bool _isIdle;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ResetIdle(animator);
+    }
 
-//     private float _IdleTime;
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!_isIdle)
+        {
+            _idleTime += Time.deltaTime;
 
-//     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-//     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-//     {
-//      ResetIdle(animator);
-//     }
+            if (_idleTime > _timeUntilIdle)
+            {
+                _isIdle = true;
+                int idleAnimation = Random.Range(1, _numberOfIdle + 1);
 
-//     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-//     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-//     {
-//        if (_isIdle == false)
-//        {
-//         _idleTime += Time.deltaTime;
+                animator.SetFloat("IdleAnimation", idleAnimation);
+            }
+        }
+        else if (stateInfo.normalizedTime % 1 > 0.98)
+        {
+            ResetIdle(animator);
+        }
+    }
 
+    private void ResetIdle(Animator animator)
+    {
+        _isIdle = false;
+        _idleTime = 0;
 
-//         if (_IdleTime > _timeUntilIdle)
-//         {
-//             _isIdle = true ;
-//             int IdleAnimation = Random.Range(1, _numberofIdle + 1);
-
-//             Animator.Setfloat("IdleAnimation", IdleAnimation);
-//         }
-//        }
-//        else if (stateInfo.normalizedTime % 1 > 0.98)
-//        {
-//         ResetIdle(animator);
-//        }
-//     }
-// private void ResetIdle(Animator animator)
-// {
-//     _isIdle = false;
-//     _IdleTime = 0;
-
-//     animator.Setfloat("IdleAnimation",0);
-// }
-
-// }
+        animator.SetFloat("IdleAnimation", 0);
+    }
+}
