6ad0c82a886b501d307b55fcc00ad9c2cb5bf804
princenizroh <princenizroh7@gmail.com>
2023-09-01 04:23:56 +0800

    fix bug room

--- Assets/script/Manager/MalikaFoodRoomManager.cs
+++ Assets/script/Manager/MalikaFoodRoomManager.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class MalikaManager : MonoBehaviour
+public class MalikaFoodRoomManager : MonoBehaviour
 {
     public static Vector3 lastCheckPointPos;
     public GameObject[] playerPrefabs;
     int characterIndex;
-
-    public static int numbOfKelereng;
-    public Text kelerengText;
     // Start is called before the first frame update
     void Start()
     {
-        lastCheckPointPos = new Vector3 (555, 235, 218);
+        lastCheckPointPos = new Vector3 (555, 235, 600);
         transform.rotation = Quaternion.identity;
         Quaternion newRotation = Quaternion.Euler(0, 180, 0);
         transform.rotation = newRotation;
@@ -22,13 +18,11 @@ public class MalikaManager : MonoBehaviour
         characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
 
         Instantiate(playerPrefabs[characterIndex], lastCheckPointPos, transform.rotation);
-
-        numbOfKelereng = PlayerPrefs.GetInt("Kelereng", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        kelerengText.text =  numbOfKelereng.ToString();
+        
     }
 }
